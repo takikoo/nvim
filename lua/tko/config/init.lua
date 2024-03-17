2badda5996386f1e602ae6ab72c00c4ef76e3877
takikoo <takiko@protonmail.com>
2024-03-17 13:14:08 +0100

    Reorganizing files

---
+++ lua/tko/config/init.lua
@@ -0,0 +1,15 @@
+-- [[ Global variables ]]
+require 'tko.globals'
+
+-- [[ Setting options ]]
+require 'tko.options'
+
+-- [[ Basic Keymaps ]]
+require 'tko.maps'
+
+-- [[ Auto commands ]]
+require 'tko.autocommands'
+
+-- [[ Lazy Plugin Manager ]]
+require 'tko.lazy-bootstrap'
+require 'tko.lazy-plugins'
