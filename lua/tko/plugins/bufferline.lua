7f0a48c93eac6262851a362ba809bd80c4574868
takikoo <takiko@protonmail.com>
2024-03-27 00:56:27 +0100

    minor adjustments

--- lua/tko/plugins/bufferline.lua
+++ lua/tko/plugins/bufferline.lua
@@ -1,5 +1,9 @@
 return {
   'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = {
+    { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+  },
   event = 'VeryLazy',
   keys = {
     { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle pin' },
