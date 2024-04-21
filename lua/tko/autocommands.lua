661380834956666e3ef627821e578447718806c1
takikoo <takiko@protonmail.com>
2024-04-22 00:59:17 +0200

    ZMK-keymaps are read as C-files

--- lua/tko/autocommands.lua
+++ lua/tko/autocommands.lua
@@ -10,3 +10,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- Add custom filetype '.keymap' and read it as a c file
+-- vim.cmd 'autocmd BufNewFile,BufRead *.keymap set filetype=c'
+vim.api.nvim_create_autocmd({'BufNewFile', 'BufRead'}, {
+  desc = 'Add custom filetype ".keymap" and read it as a c file',
+  group = vim.api.nvim_create_augroup('qmk-keymap-filetype', { clear = true }),
+  pattern = '*.keymap',
+  callback = function()
+    vim.bo.filetype = 'c'
+  end,
+})
